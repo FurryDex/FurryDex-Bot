04e62d54a3fea0f7a7b2aba4ea7d4780ae7e13ef
flyzar73 <61349213+flyzar73@users.noreply.github.com>
2024-07-09 16:28:28 +0200

    give information added

--- utils/functions/card.js
+++ utils/functions/card.js
@@ -9,34 +9,41 @@ function cardEmbed(cardId, locale) {
   //let creator = client.users.fetch(originalCardF.author);
   let date = new Date(cardF.date);
   let description = locales.embed.description[locale] ?? locales.embed.description.default;
+  description = description
+    .replace("%emoji_1%", "<:atlanta_crown:598174064183607317>")
+    .replace("%author%", originalCardF.author)
+    .replace("%emoji_2%", "<:atlanta_id:598162717232332811>")
+    .replace("%id%", cardF.id)
+    .replace("%emoji_3%", "🪪")
+    .replace("%name%", originalCardF.name)
+    .replace("%emoji_4%", "📅")
+    .replace("%time%", `${time(date, TimestampStyles.LongDateTime)} (${time(date, TimestampStyles.RelativeTime)})`)
+    .replace("%emoji_5%", "❤️")
+    .replace(
+      "%live%",
+      cardF.live < 0 ? originalCardF.live - (originalCardF.live * cardF.live.replace("-", "")) / 100 : originalCardF.live + (originalCardF.live * cardF.live) / 100
+    ) //cardF.live < 0 ? originalCardF.live-(originalCardF.live*cardF.live/100) : originalCardF.live+(originalCardF.live*cardF.live/100)
+    .replace("%live_2%", cardF.live)
+    .replace("%emoji_6%", " <:atlanta_minecraft:598170502963396620>")
+    .replace(
+      "%attacks%",
+      cardF.attacks < 0
+        ? originalCardF.attacks - (originalCardF.attacks * cardF.attacks.replace("-", "")) / 100
+        : originalCardF.attacks + (originalCardF.attacks * cardF.attacks) / 100
+    ) //cardF.attacks < 0 ? originalCardF.attacks-(originalCardF.attacks*cardF.attacks/100) : originalCardF.attacks+(originalCardF.attacks*cardF.attacks/100)
+    .replace("%attacks_2%", cardF.attacks);
+  if (cardF.gived) {
+    description.replace(
+      "%gived%",
+      `\n${(locales.embed.giveBy[locale] ?? locales.embed.giveBy.default).replace("%emoji%", "<:atlanta_add:598176235700355083>").replace("%author%", cardF.giveBy)}\n`
+    );
+  } else {
+    description.replace("%gived", "");
+  }
   let embed = new EmbedBuilder()
     .setColor(originalCardF.color)
     .setTitle(`${originalCardF.name}`)
-    .setDescription(
-      description
-        .replace("%emoji_1%", "<:atlanta_crown:598174064183607317>")
-        .replace("%author%", originalCardF.author)
-        .replace("%emoji_2%", "<:atlanta_id:598162717232332811>")
-        .replace("%id%", cardF.id)
-        .replace("%emoji_3%", "🪪")
-        .replace("%name%", originalCardF.name)
-        .replace("%emoji_4%", "📅")
-        .replace("%time%", `${time(date, TimestampStyles.LongDateTime)} (${time(date, TimestampStyles.RelativeTime)})`)
-        .replace("%emoji_5%", "❤️")
-        .replace(
-          "%live%",
-          cardF.live < 0 ? originalCardF.live - (originalCardF.live * cardF.live.replace("-", "")) / 100 : originalCardF.live + (originalCardF.live * cardF.live) / 100
-        ) //cardF.live < 0 ? originalCardF.live-(originalCardF.live*cardF.live/100) : originalCardF.live+(originalCardF.live*cardF.live/100)
-        .replace("%live_2%", cardF.live)
-        .replace("%emoji_6%", " <:atlanta_minecraft:598170502963396620>")
-        .replace(
-          "%attacks%",
-          cardF.attacks < 0
-            ? originalCardF.attacks - (originalCardF.attacks * cardF.attacks.replace("-", "")) / 100
-            : originalCardF.attacks + (originalCardF.attacks * cardF.attacks) / 100
-        ) //cardF.attacks < 0 ? originalCardF.attacks-(originalCardF.attacks*cardF.attacks/100) : originalCardF.attacks+(originalCardF.attacks*cardF.attacks/100)
-        .replace("%attacks_2%", cardF.attacks)
-    )
+    .setDescription(description)
     //%emoji_1%, <:atlanta_crown:598174064183607317> | %author%, originalCardF.author | %emoji_2%, <:atlanta_id:598162717232332811> | %id%, cardF.id | %emoji_3%, 🪪
     //%name%`, originalCardF.name | %emoji_4%, 📅 | %time%, ${time(date, TimestampStyles.LongDateTime)} (${time(date, TimestampStyles.RelativeTime)}) | %emoji_5%, ❤️
     //%live%, originalCardF.live | %live_2%, cardF.live | %emoji_6%, <:atlanta_minecraft:598170502963396620> | %attacks%, originalCardF.attacks | %attacks_2%, cardF.attacks
