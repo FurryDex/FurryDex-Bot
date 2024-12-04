d94a2dcc24d216729dc8e78a771fd08cae506d4a
flyzar73 <61349213+flyzar73@users.noreply.github.com>
2024-12-04 17:31:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -136,7 +136,7 @@ client.on('messageCreate', (message) => {
 let callAmount = 0;
 process.on('SIGINT', function () {
 	if (callAmount < 1) {
-		Console.log(`✅ The server has been stopped`, 'Shutdown information', 'This shutdown was initiated by CTRL+C.');
+		console.log(`✅ The server has been stopped`, 'Shutdown information', 'This shutdown was initiated by CTRL+C.');
 		setTimeout(() => process.exit(0), 1000);
 	}
 
