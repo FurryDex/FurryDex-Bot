db220b5fa1c57d24437860fe371b514ba3d79dc6
flyzar73 <61349213+flyzar73@users.noreply.github.com>
2024-12-04 20:37:35 +0100

    Update spawn.js

--- utils/functions/spawn.js
+++ utils/functions/spawn.js
@@ -221,7 +221,7 @@ function filtrerCartesParServeur(client, enableFilter, cartes, guildId) {
 		// Filtre les cartes en vérifiant si l'authorId (converti en chaîne) est présent parmi les membres
 		let cartesFiltrees;
 
-		console.log(membres.has(carte.authorId.toString());)
+		console.log(membres.has(carte.authorId.toString()));
 		if (enableFilter) {
 			cartesFiltrees = cartes.filter((carte) => {
 				membres.has(carte.authorId.toString());
