135b4b8c2e3e42104b27af8545154ca18501a998
flyzar73 <61349213+flyzar73@users.noreply.github.com>
2024-12-15 16:01:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ const client = new Client({
 	partials: [Partials.User, Partials.Channel, Partials.GuildMember, Partials.Message, Partials.Reaction, Partials.GuildScheduledEvent, Partials.ThreadMember],
 });
 const Logger = require('./utils/Logger');
-const debug = false;
+const debug = true;
 
 if (!require('./config.json').shard) require('./api/server');
 //require('./api/server');
