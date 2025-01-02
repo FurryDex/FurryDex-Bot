f79d7fb7d30fe395f40df55dcb87c79d92b38b75
flyzar73 <61349213+flyzar73@users.noreply.github.com>
2025-01-02 14:59:02 +0100

    Update card.js

--- utils/functions/card.js
+++ utils/functions/card.js
@@ -74,7 +74,7 @@ async function cardEmbed(client, cardId, locale) {
 	}
 	let embed = new EmbedBuilder()
 		.setColor(color)
-		.setTitle(`${originalCardF.name}`)
+		.setTitle(`${originalCardF.emoji} ${originalCardF.name}`)
 		.setDescription(description)
 		//%emoji_1%, <:atlanta_crown:598174064183607317> | %author%, originalCardF.author | %emoji_2%, <:atlanta_id:598162717232332811> | %id%, cardF.id | %emoji_3%, 🪪
 		//%name%`, originalCardF.name | %emoji_4%, 📅 | %time%, ${time(date, TimestampStyles.LongDateTime)} (${time(date, TimestampStyles.RelativeTime)}) | %emoji_5%, ❤️
