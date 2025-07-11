4c4acf981523ebadb96be9b08815d404c46a9e92
flyzar73 <61349213+flyzar73@users.noreply.github.com>
2025-07-11 12:23:14 +0200

    Update catch.js

--- modals/spawn/catch.js
+++ modals/spawn/catch.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, ButtonBuilder } = require('discord.js');
+const { MessageFlags } = require('discord.js');
 const uid = function () {
 	return Date.now().toString(36) + Math.random().toString(36).substr(2);
 };
